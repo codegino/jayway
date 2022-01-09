8b63fd9ad3b77d706375f1411de2a1fc1885b2d9
codegino <carloginocatapang@gmail.com>
2022-01-09 19:27:15 +0800

    refactor: removed unused code and scripts

--- src/utils/color-generator.ts
+++ src/utils/color-generator.ts
@@ -1,4 +1,4 @@
 export function generateRandomColor(): string {
   // Only generate light colors to ensure text is easy to read
-  return `hsl(${Math.random() * 360}, 100%, 75%)`;
+  return `hsl(${Math.random() * 360}, 100%, 85%)`;
 }
